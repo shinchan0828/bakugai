14595aad66f92b6f1ee488a764beef06b4f88012
04_Yamamoto_Shinji <shinji.yamamoto0828@gmail.com>
2018-04-14 07:57:18 +0000

    first commit

---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,6 @@
+class ToppagesController < ApplicationController
+  #before_action :authenticate_user!
+  
+  def index
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  validate :seller_or_buyer
+    
+  def seller_or_buyer
+      if buyer_flag == seller_flag
+          errors.add(:buyer_flag, "どちらか選択してください。")
+      end
+  end
+  
+  after_save :set_role
+  def set_role
+    if self.buyer_flag?
+      self.add_role "buyer"
+    elsif self.seller_flag?
+      self.add_role "seller"
+    end
+  end
+         
+  mount_uploader :image, ImageUploader
+end
