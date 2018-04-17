981e6cad3c08352fa351ce6643abb73d23967260
04_Yamamoto_Shinji <shinji.yamamoto0828@gmail.com>
2018-04-17 19:04:57 +0000

    role error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :buyer_flag, :seller_flag, :image])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :profile, :image])
     end
   
     protect_from_forgery with: :exception
